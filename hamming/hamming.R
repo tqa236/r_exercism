a044a15382511ae51f3b8483ba2a13aec364b8da
TQA <trinhquocanh94@gmail.com>
2019-01-28 19:44:50 +0100

    Download new exercises.

--- hamming/hamming.R
+++ hamming/hamming.R
@@ -4,9 +4,6 @@ hamming <- function(strand1, strand2) {
   if (nchar(strand1) != nchar(strand2)){
     stop("2 ADN do not have equal length.")
   }
-  else if ((nchar(strand1) == 0) || (nchar(strand2) == 0)){
-    0
-  }
   else {
     strand1_split <- strsplit(strand1, "")[[1]]
     strand2_split <- strsplit(strand2, "")[[1]]
