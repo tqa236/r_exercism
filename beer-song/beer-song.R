c4c42b62f00c50b43cdfccd710c3cf3608c1f140
TQA <trinhquocanh94@gmail.com>
2019-02-11 22:41:53 +0100

    Solve two fer

---
+++ beer-song/beer-song.R
@@ -0,0 +1,7 @@
+lyrics <- function(first, last) {
+  
+}
+
+verse <- function(number) {
+  sprintf("%s bottles of beer on the wall, %s bottles of beer.\nTake one down and pass it around, %s bottles of beer on the wall.\n", number, number, number - 1)
+}
--- bob/bob.R
+++ bob/bob.R
@@ -1,8 +1,7 @@
-library(stringr)
-
 bob <- function(input) {
+  input <- trimws(input)
   if (grepl("^\\s*$", input)) "Fine. Be that way!"
-  else if (str_sub(trimws(input, "r"), start = -1) == "?"){
+  else if (substring(input, nchar(input)) == "?"){
     if ((input == toupper(input)) && (unlist(gregexpr("[A-Z]", input))[1] > 0)) "Calm down, I know what I'm doing!"
     else "Sure."
   }
