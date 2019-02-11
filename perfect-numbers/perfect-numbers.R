ab1bd4ba16ff12deb22d35d30e4b0de2da464a96
TQA <trinhquocanh94@gmail.com>
2019-02-11 01:04:24 +0100

    Refactoring

--- perfect-numbers/perfect-numbers.R
+++ perfect-numbers/perfect-numbers.R
@@ -1,13 +1,13 @@
 number_type <- function(n){
-  square_root = round(sqrt(n))
-  numbers = 1:square_root
-  
-  smallDivisors <- numbers[n%%numbers == 0]
-  bigDivisors = n/smallDivisors
+  square_root <- round(sqrt(n))
+  numbers <- 1:square_root
 
-  aliquot = sum(smallDivisors) + sum(bigDivisors)
-  if (square_root^2 == n) aliquot = aliquot - square_root
-  
+  small_divisors <- numbers[n%%numbers == 0]
+  big_divisors <- n / small_divisors
+
+  aliquot <- sum(small_divisors) + sum(big_divisors)
+  if (square_root ^ 2 == n) aliquot <- aliquot - square_root
+
   if (aliquot > 2 * n)  return("abundant")
   if (aliquot == 2 * n) return("perfect")
   "deficient"
--- rna-transcription/rna-transcription.R
+++ rna-transcription/rna-transcription.R
@@ -1,10 +1,10 @@
 replace <- function(strand,rna_map){
-  if (!(strand %in% c("C","G","A","T"))) throws("Invalid ADN")
+  if (!(strand %in% c("C", "G", "A", "T"))) throws("Invalid ADN")
   rna_map[strand]
 }
 
 to_rna <- function(dna) {
-  rna_map <- list("C"="G","G"="C","T"="A","A"="U")
+  rna_map <- list("C" = "G", "G" = "C", "T" = "A", "A" = "U")
   dnal <- unlist(strsplit(dna, "")[[1]])
   paste(sapply(dnal, replace, rna_map = rna_map), collapse='')
 }
