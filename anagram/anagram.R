befd872040a523ae8cdb69363cac6434b4e2a43c
TQA <trinhquocanh94@gmail.com>
2019-02-12 21:21:59 +0100

    Solve anagram

--- anagram/anagram.R
+++ anagram/anagram.R
@@ -1,6 +1,6 @@
 anagram <- function(subject, candidates) {
-  subject_v = sort(unlist(strsplit(tolower(subject), "")))
-  is_anagram <- sapply(candidates, function(x){(tolower(x) != tolower(subject)) && identical(subject_v,sort(unlist(strsplit(tolower(x), ""))))})
+  subject_v <- sort(unlist(strsplit(tolower(subject), "")))
+  is_anagram <- sapply(candidates, function(x){(tolower(x) != tolower(subject)) && identical(subject_v, sort(unlist(strsplit(tolower(x), ""))))})
   anagram_list <- candidates[is_anagram]
   
   if (identical(anagram_list, character(0))) c()
