6254a5963d5dc6e260f244ca426aa30fa5e5343f
TQA <trinhquocanh94@gmail.com>
2019-02-13 14:19:52 +0100

    Solve anagram

--- anagram/anagram.R
+++ anagram/anagram.R
@@ -1,8 +1,12 @@
+to_letter <- function(word){
+  sort(unlist(strsplit(tolower(word), "")))
+}
+
 anagram <- function(subject, candidates) {
-  subject_v <- sort(unlist(strsplit(tolower(subject), "")))
-  is_anagram <- sapply(candidates, function(x){(tolower(x) != tolower(subject)) && identical(subject_v, sort(unlist(strsplit(tolower(x), ""))))})
+  subject_v <- to_letter(subject)
+  is_anagram <- sapply(candidates, function(x){(tolower(x) != tolower(subject)) && identical(subject_v, to_letter(x))})
   anagram_list <- candidates[is_anagram]
   
-  if (identical(anagram_list, character(0))) c()
+  if (length(anagram_list) == 0) c()
   else anagram_list
 }
--- beer-song/beer-song.R
+++ beer-song/beer-song.R
@@ -3,5 +3,8 @@ lyrics <- function(first, last) {
 }
 
 verse <- function(number) {
-  sprintf("%s bottles of beer on the wall, %s bottles of beer.\nTake one down and pass it around, %s bottles of beer on the wall.\n", number, number, number - 1)
+  plural = "s"
+  if (number == 2) plural = ""
+  bottles = paste0(as.character(number), " bottle", plural)
+  sprintf("%s of beer on the wall, %d bottles of beer.\nTake one down and pass it around, %d bottle%s of beer on the wall.\n", bottles, number, number - 1, plural)
 }
