80acfddf18dc3f2bd3ffe134f2270a3e7477a6c2
Quoc Anh Trinh <trinhquocanh94@gmail.com>
2019-10-20 20:00:07 +0200

    Refactor fizz buzz

--- fizz-buzz/fizz-buzz.R
+++ fizz-buzz/fizz-buzz.R
@@ -1,8 +1,8 @@
 convert <- function(input){
-  if (input %% 15 == 0) return("Fizz Buzz")
-  else if (input %% 3 == 0) return("Fizz")
-  else if (input %% 5 == 0) return("Buzz")
-  else return(toString(input))
+  if (input %% 15 == 0) "Fizz Buzz"
+  else if (input %% 3 == 0) "Fizz"
+  else if (input %% 5 == 0) "Buzz"
+  else toString(input)
   
 }
 
